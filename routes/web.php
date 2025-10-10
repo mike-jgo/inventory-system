29f9f4551f429bc0a9aed756a896b13a039b24d0
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-10-10 20:46:46 +0800

    Modified web routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ItemController;
 
-Route::get('/', function () {
-    return Inertia::render('Product');
-})->name('home');
+Route::get('/', [ItemController::class, 'index'])->name('items.index');
+Route::post('/items', [ItemController::class, 'store'])->name('items.store');
+Route::put('/items/{item}', [ItemController::class, 'update'])->name('items.update');
+Route::delete('/items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

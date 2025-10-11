96ca52d1ee98f6bc814571a53f96b1ded9ce18be
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-10-11 21:22:42 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [ItemController::class, 'index'])->name('items.index');
 Route::post('/items', [ItemController::class, 'store'])->name('items.store');
 Route::put('/items/{item}', [ItemController::class, 'update'])->name('items.update');
 Route::delete('/items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
+
+Route::resource('categories', CategoryController::class)->except(['show', 'create', 'edit']);
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

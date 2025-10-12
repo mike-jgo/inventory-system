2372b15371314f20a11facb65029ba93dd778b47
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-10-13 00:45:52 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,54 @@
 import prettier from 'eslint-plugin-prettier';
 import vue from 'eslint-plugin-vue';
 import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...vue.configs['flat/recommended'],
-  {
-    ignores: ['node_modules/**']
-  },
-  {
-    files: ['**/*.js', '**/*.vue'],
-    languageOptions: {
-      globals: {
-        _: 'readonly',
-        route: 'readonly',
-        browser: true,
-        node: true
-      },
-      parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-        parser: tsparser
-      }
-    },
-    plugins: {
-      prettier
-    },
-    rules: {
-      'vue/html-indent': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          singleAttributePerLine: true
-        }
-      ],
-      'vue/require-default-prop': 'off',
-      'vue/singleline-html-element-content-newline': 'off',
-      'vue/multi-word-component-names': 'off',
-      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-      'vue/max-attributes-per-line': [
-        'error',
-        {
-          singleline: {
-            max: 1
-          },
-          multiline: {
-            max: 1
-          }
-        }
-      ]
-    }
-  }
+	js.configs.recommended,
+	tseslint.configs.recommended, // ✅ no spread, just include the object
+	vue.configs['flat/recommended'],
+
+	{
+		ignores: ['node_modules/**']
+	},
+
+	{
+		files: ['**/*.js', '**/*.vue', '**/*.ts'],
+		languageOptions: {
+			globals: {
+				_: 'readonly',
+				route: 'readonly',
+				browser: true,
+				node: true
+			},
+			parser: tseslint.parser, // ✅ parser comes from `typescript-eslint`
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module'
+			}
+		},
+		plugins: {
+			prettier
+		},
+		rules: {
+			'vue/html-indent': 'off',
+			'prettier/prettier': [
+				'error',
+				{
+					singleAttributePerLine: true
+				}
+			],
+			'vue/require-default-prop': 'off',
+			'vue/singleline-html-element-content-newline': 'off',
+			'vue/multi-word-component-names': 'off',
+			'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+			'vue/max-attributes-per-line': [
+				'error',
+				{
+					singleline: { max: 1 },
+					multiline: { max: 1 }
+				}
+			]
+		}
+	}
 ];

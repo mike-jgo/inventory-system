3a0e020c013384336f4cd24bb82cc82d0936d2bf
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-10-13 00:22:03 +0800

    Modify Formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,57 @@
-import prettier from 'eslint-config-prettier/flat';
+import prettier from 'eslint-plugin-prettier';
 import vue from 'eslint-plugin-vue';
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
-
-export default defineConfigWithVueTs(
-    vue.configs['flat/essential'],
-    vueTsConfigs.recommended,
-    {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'resources/js/components/ui/*'],
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    ignores: ['node_modules/**']
+  },
+  {
+    files: ['**/*.js', '**/*.vue'],
+    languageOptions: {
+      globals: {
+        _: 'readonly',
+        route: 'readonly',
+        browser: true,
+        node: true
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        parser: tsparser
+      }
     },
-    {
-        rules: {
-            'vue/multi-word-component-names': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
-        },
+    plugins: {
+      prettier
     },
-    prettier,
-);
+    rules: {
+      'vue/html-indent': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          singleAttributePerLine: true
+        }
+      ],
+      'vue/require-default-prop': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: {
+            max: 1
+          },
+          multiline: {
+            max: 1
+          }
+        }
+      ]
+    }
+  }
+];

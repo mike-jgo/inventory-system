de7bcd106cda78146c4c69f2b4087b2fe9289f50
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-12-07 01:49:48 +0800

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
 
 class Item extends Model
 {
@@ -15,5 +16,13 @@ class Item extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('item')
+            ->logFillable()
+            ->setDescriptionForEvent(fn(string $eventName) => "Item has been {$eventName}");
     }
 }

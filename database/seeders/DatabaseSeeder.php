430f86af2f5370dd42084bc5f83eac7ce7e5bdd6
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-12-07 13:33:01 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create()
 
-        $this->call(CategorySeeder::class);
+        $this->call([
+            CategorySeeder::class,
+            RolePermissionSeeder::class,
+        ]);
     }
 }

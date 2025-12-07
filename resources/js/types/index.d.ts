ed54eec5657c42c2302c392bb7a73ca4695f2ca9
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-12-07 13:33:19 +0800

    Update index.d.ts

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -32,6 +32,8 @@ export interface User {
 	email_verified_at: string | null;
 	created_at: string;
 	updated_at: string;
+    roles?: string[];
+    can?: Record<string, boolean>;
 }
 
 export type BreadcrumbItemType = BreadcrumbItem;

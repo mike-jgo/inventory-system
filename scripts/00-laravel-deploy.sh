b3ecd50da0f64a666ae030c1b6b70f249b8e169d
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-01-01 16:31:19 +0800

    Update docker

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "generating application key..."
+php artisan key:generate --show
+
 echo "Caching config..."
 php artisan config:cache
 
@@ -11,3 +14,6 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Running seeders..."
+php artisan db:seed

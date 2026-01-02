c04554fd7b101dc3e59c7ce6afc5923b6341dae2
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-01-02 14:12:03 +0800

    Update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'logo' => config('app.logo'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user() ? array_merge($request->user()->toArray(), [

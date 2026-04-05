83ee4c355bc331f64c2a7c9af964ccacf55b17ac
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-04-06 06:42:51 +0800

    Update Register.vue

--- resources/js/pages/Auth/Register.vue
+++ resources/js/pages/Auth/Register.vue
@@ -26,13 +26,10 @@ const submit = () => {
 					<input
 						v-model="form.name"
 						type="text"
-						class="w-full border-gray-300 rounded-lg border-1 px-2"
+						class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
 						required
 					/>
-					<div
-						v-if="form.errors.name"
-						class="text-red-600 text-sm mt-1"
-					>
+					<div v-if="form.errors.name" class="text-red-600 text-sm mt-1">
 						{{ form.errors.name }}
 					</div>
 				</div>
@@ -43,13 +40,10 @@ const submit = () => {
 					<input
 						v-model="form.email"
 						type="email"
-						class="w-full border-gray-300 rounded-lg border-1 px-2"
+						class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
 						required
 					/>
-					<div
-						v-if="form.errors.email"
-						class="text-red-600 text-sm mt-1"
-					>
+					<div v-if="form.errors.email" class="text-red-600 text-sm mt-1">
 						{{ form.errors.email }}
 					</div>
 				</div>
@@ -60,13 +54,16 @@ const submit = () => {
 					<input
 						v-model="form.password"
 						type="password"
-						class="w-full border-gray-300 rounded-lg border-1 px-2"
+						class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
 						required
 					/>
-					<div
-						v-if="form.errors.password"
-						class="text-red-600 text-sm mt-1"
-					>
+					<ul class="mt-1 text-xs text-gray-500 space-y-0.5 list-disc list-inside">
+						<li>At least 8 characters</li>
+						<li>At least one uppercase letter</li>
+						<li>At least one number</li>
+						<li>At least one special character (e.g. !@#$%)</li>
+					</ul>
+					<div v-if="form.errors.password" class="text-red-600 text-sm mt-1">
 						{{ form.errors.password }}
 					</div>
 				</div>
@@ -77,20 +74,17 @@ const submit = () => {
 					<input
 						v-model="form.password_confirmation"
 						type="password"
-						class="w-full border-gray-300 rounded-lg border-1 px-2"
+						class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
 						required
 					/>
-					<div
-						v-if="form.errors.password_confirmation"
-						class="text-red-600 text-sm mt-1"
-					>
+					<div v-if="form.errors.password_confirmation" class="text-red-600 text-sm mt-1">
 						{{ form.errors.password_confirmation }}
 					</div>
 				</div>
 
 				<button
 					type="submit"
-					class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
+					class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition disabled:opacity-50"
 					:disabled="form.processing"
 				>
 					Register
@@ -99,10 +93,7 @@ const submit = () => {
 
 			<p class="mt-4 text-center text-sm">
 				Already have an account?
-				<Link
-					:href="route('login')"
-					class="text-blue-500 hover:underline"
-				>
+				<Link :href="route('login')" class="text-blue-500 hover:underline">
 					Login
 				</Link>
 			</p>

61a1fdc47e0a79e205312b9cb5acce391ddd56b4
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-04-06 06:42:44 +0800

    Update Index.vue

--- resources/js/pages/ActivityLog/Index.vue
+++ resources/js/pages/ActivityLog/Index.vue
@@ -146,20 +146,9 @@ const saveRemarks = (remarks: string) => {
 };
 
 const formatDate = (dateString: string) => {
-	const date = new Date(dateString);
-	return (
-		date.getFullYear() +
-		'-' +
-		String(date.getMonth() + 1).padStart(2, '0') +
-		'-' +
-		String(date.getDate()).padStart(2, '0') +
-		' ' +
-		String(date.getHours()).padStart(2, '0') +
-		':' +
-		String(date.getMinutes()).padStart(2, '0') +
-		':' +
-		String(date.getSeconds()).padStart(2, '0')
-	);
+	return new Date(dateString).toLocaleString('sv-SE', {
+		timeZone: 'Asia/Manila',
+	});
 };
 
 // Filter configuration
